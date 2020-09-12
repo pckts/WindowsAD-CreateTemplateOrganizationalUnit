bfe513352a5c02137b49cdf8a906f1be291bd1e7
Packet <atl@pck.dk>
2020-09-12 02:45:28 +0200

    Add files via upload

---
+++ CreateOUs.ps1
@@ -0,0 +1,44 @@
+﻿
+
+
+echo "+---------------------------+"
+echo "|       Company Name        |"
+echo "+---------------------------+"
+echo ""
+echo 'What is the company name?'
+echo "(No spaces)"
+echo ""
+$CompanyName = read-host 'Company Name'
+cls
+
+echo "+---------------------------+"
+echo "|          Language         |"
+echo "+---------------------------+"
+echo ""
+echo "Which language should the OUs be in?"
+echo ""
+$LangSelect = Read-Host "(E)nglish/(D)anish"
+if ($LangSelect -eq 'e' -or $LangSelect -eq 'english')
+{
+    cls
+    New-ADOrganizationalUnit -Name "$CompanyName" -Path "DC=domain,DC=local" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name "Users" -Path "OU=$CompanyName,DC=domain,DC=local" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name "Admin" -Path "OU=Users,OU=$CompanyName,DC=domain,DC=local" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name "User" -Path "OU=Users,OU=$CompanyName,DC=domain,DC=local" -ProtectedFromAccidentalDeletion $False
+    cls
+}
+if ($LangSelect -eq 'd' -or $LangSelect -eq 'danish')
+{
+    cls
+    New-ADOrganizationalUnit -Name "$CompanyName" -Path "DC=domain,DC=local" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name "Brugere" -Path "OU=$CompanyName,DC=domain,DC=local" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name "Admin" -Path "OU=Brugere,OU=$CompanyName,DC=domain,DC=local" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name "Bruger" -Path "OU=Brugere,OU=$CompanyName,DC=domain,DC=local" -ProtectedFromAccidentalDeletion $False
+    cls
+}
+if ($LangSelect -ne 'e' -and $LangSelect -ne 'd' -and $LangSelect -ne 'english' -and $LangSelect -ne 'danish')
+{
+    cls
+    echo "Please choose a language"
+    pause
+}
